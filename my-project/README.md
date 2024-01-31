ff29c932ad3a94cba6fada3c43a8e7c4ec6112ae
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2024-01-31 08:36:55 -0300

    up

--- my-project/README.md
+++ my-project/README.md
@@ -1,4 +1,4 @@
-# TesteRota
+# MyProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.12.
 
--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
